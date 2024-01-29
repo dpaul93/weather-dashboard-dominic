7243b60808d95e5d7e86d00208cd13d62433e211
Dominic <dominicepaul@gmail.com>
2024-01-29 23:40:31 +0000

    i've broken it. Need to investigate using console and debugger

--- assets/script.js
+++ assets/script.js
@@ -12,11 +12,48 @@ $(document).ready(function () {
         fetch(queryURL1)
         .then((Response) => Response.json())
         .then((data) => {
-            const lat = data.[0].lat;
-            const lon = data.[0].lon;
+            const lat = data[0].lat;
+            const lon = data[0].lon;
             const country = data[0].country;
             console.log(data);
 
+            const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
 
+            fetch(queryURL2)
+        .then((response) => response.json())
+        .then((data) => {
+            function displayWeatherData() {
+            const forecastContainer = $("#weatherForecast");
+
+            const groupedByDay = {};
+            data.list.foreEach((forecast) => {
+                const datOfWeek = dayjs(forcast.dt_text).format("dddd DD");
+
+                if (dayjs(forecast.dt_txt).isAfter(dayjs(), "day")) {
+                    if (!groupedByDay[dayOfWeek]) {
+                    groupedByDay[dayOfWeek] = forecast;
+                    }
+                }
+                }); 
+
+                for (const dayOfWeek in groupedByDay) {
+                    const dayContainer = $(
+                    `<div class="day-container"><h3>${dayOfWeek}</h3></div>`
+                    );
+                    const forecast = groupedByDay[dayOfWeek];
+    
+                    const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+                    const tempK = forecast.main.temp;
+                    const tempCel = `${Math.round(tempK - 273.15)}°C`;
+    
+                    dayContainer.append(
+                    `<div><img src="${imageUrl}" alt="Weather Icon"><br> ${forecast.weather[0].description} <br>Temp: ${tempCel} |<br> Wind: ${forecast.wind.speed} m/ph | <br>Humidity: ${forecast.main.humidity}%</div> <hr>`
+                    );
+
+                    forecastContainer.append(dayContainer);
+                }
+                }
+            
+            }
         })
-    });
+        );

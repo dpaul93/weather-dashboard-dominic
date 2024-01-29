b2e2633f13df0f6866610875c3783a7e49083b89
Dominic <dominicepaul@gmail.com>
2024-01-29 23:21:10 +0000

    fetch for api created

--- assets/script.js
+++ assets/script.js
@@ -9,5 +9,14 @@ $(document).ready(function () {
         const key = "33180eb70b4778f17b8aaca9485bd8ad";
         const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`;
 
-        
+        fetch(queryURL1)
+        .then((Response) => Response.json())
+        .then((data) => {
+            const lat = data.[0].lat;
+            const lon = data.[0].lon;
+            const country = data[0].country;
+            console.log(data);
+
+
+        })
     });

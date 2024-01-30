f297a913f34396ab09a87484911b894e8c5a4b13
Dominic <dominicepaul@gmail.com>
2024-01-30 23:33:56 +0000

    trying to work on 5 day forcast still. some styling added and readme updated.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # weather-dashboard-dominic
 weather app to display 5 day forecast for selected cities
+This task was to create a 5 day weather forcaster using API's and local storage. 
+
+The challenge of this task was utilising the API with .fetch commands and this was definately challenging. 
+
+I am aware that the weather dashboard is incomplete as it is at the moment, this is because I am still working on the logic to correctly display the 5 day forcast. I am continuously looking at the code to try and develope it into a finished product. 
+
+Please see some snippets of the current application.
+
+![Alt text](image.png)
+
+![Alt text](image-1.png)
+
+![Alt text](image-2.png)
+
+https://github.com/dpaul93/weather-dashboard-dominic
+
+https://dpaul93.github.io/weather-dashboard-dominic/
--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
-    displayCities(savedCities);
+const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+displayCities(savedCities);
 
-    $("#search-form").submit(function (event) {
-        event.preventDefault();
-    
-        const cityname = $("#search-input").val();
-        const key = "33180eb70b4778f17b8aaca9485bd8ad";
-        const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`;
+$("#search-form").submit(function (event) {
+    event.preventDefault();
 
-        fetch(queryURL1)
-        .then((Response) => Response.json())
-        .then((data) => {
-            const lat = data[0].lat;
-            const lon = data[0].lon;
-            const country = data[0].country;
-            console.log(data);
+    const cityname = $("#search-input").val();
+    const key = "33180eb70b4778f17b8aaca9485bd8ad";
+    const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`;
 
-            const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+    fetch(queryURL1)
+    .then((Response) => Response.json())
+    .then((data) => {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        const country = data[0].country;
+        console.log(data);
 
-            fetch(queryURL2)
+        const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+
+        fetch(queryURL2)
         .then((response) => response.json())
         .then((data) => {
             function displayWeatherData() {
@@ -30,31 +30,31 @@ $(document).ready(function () {
                 const datOfWeek = dayjs(forcast.dt_text).format("dddd DD");
 
                 if (dayjs(forecast.dt_txt).isAfter(dayjs(), "day")) {
-                    if (!groupedByDay[dayOfWeek]) {
+                if (!groupedByDay[dayOfWeek]) {
                     groupedByDay[dayOfWeek] = forecast;
-                    }
                 }
-                }); 
+                }
+            });
 
-                for (const dayOfWeek in groupedByDay) {
-                    const dayContainer = $(
-                    `<div class="day-container"><h3>${dayOfWeek}</h3></div>`
-                    );
-                    const forecast = groupedByDay[dayOfWeek];
-    
-                    const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
-                    const tempK = forecast.main.temp;
-                    const tempCel = `${Math.round(tempK - 273.15)}°C`;
-    
-                    dayContainer.append(
-                    `<div><img src="${imageUrl}" alt="Weather Icon"><br> ${forecast.weather[0].description} <br>Temp: ${tempCel} |<br> Wind: ${forecast.wind.speed} m/ph | <br>Humidity: ${forecast.main.humidity}%</div> <hr>`
-                    );
+            for (const dayOfWeek in groupedByDay) {
+                const dayContainer = $(
+                `<div class="day-container"><h3>${dayOfWeek}</h3></div>`
+                );
+                const forecast = groupedByDay[dayOfWeek];
 
-                    forecastContainer.append(dayContainer);
-                }
-                }
-            
-                const currentDate = dayjs().format("dddd DD MMM YYYY | HH:mm");
+                const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+                const tempK = forecast.main.temp;
+                const tempCel = `${Math.round(tempK - 273.15)}°C`;
+
+                dayContainer.append(
+                `<div><img src="${imageUrl}" alt="Weather Icon"><br> ${forecast.weather[0].description} <br>Temp: ${tempCel} |<br> Wind: ${forecast.wind.speed} m/ph | <br>Humidity: ${forecast.main.humidity}%</div> <hr>`
+                );
+
+                forecastContainer.append(dayContainer);
+            }
+            }
+
+            const currentDate = dayjs().format("dddd DD MMM YYYY | HH:mm");
             const txt1 = $("<h2>").text(currentDate);
             $("#today").append(txt1);
             const location = `${cityname}, ${country}`;
@@ -64,14 +64,12 @@ $(document).ready(function () {
             );
             $("#today").append(locationParagraph, description0);
 
-
             const imageUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
             const image = $("<img>")
             .attr("src", imageUrl)
             .attr("alt", "Weather Icon");
             $("#today").append(image, "<hr>");
 
-            
             const tempKelvin = data.list[0].main.temp;
             const tempC = $("<p>").text(
             `Temp: ${Math.round(tempKelvin - 273.15)}°C`
@@ -85,7 +83,7 @@ $(document).ready(function () {
             displayWeatherData();
         });
     })
-    .catch((error)=> {
+    .catch((error) => {
         console.log(error);
     });
 
@@ -98,89 +96,80 @@ $(document).ready(function () {
 
     displayCities(savedCities);
     $("#search-input").val("");
-        });
+});
+function displayCities(cities) {
+    const history = $("#history");
+    history.empty();
 
+    cities.forEach(function (city, index) {
+    const cityButton = $(
+        '<button class="btn-secondary city-button"></button>'
+    )
+        .text(city)
+        .attr("data-index", index);
+    const deleteButton = $('<span class="deleteButton">Delete</span>');
 
-        function displayCities(cities) {
-            const history = $("#history");
-            history.empty(); 
-        
-            cities.forEach(function (city, index) {
-            const cityButton = $(
-                '<button class="btn-secondary city-button"></button>'
-            )
-                .text(city)
-                .attr("data-index", index);
-            const deleteButton = $('<span class="deleteButton">Delete</span>');
-        
-            const listItem = $("<li></li>").append(cityButton).append(deleteButton);
-            history.append(listItem);
-        
-            
-            deleteButton.click(function () {
-                deleteCity(index);
-            });
+    const listItem = $("<li></li>").append(cityButton).append(deleteButton);
+    history.append(listItem);
 
-            cityButton.click(function () {
-                const clickedCity = $(this).text();
-                console.log("Clicked City:", clickedCity);
-                // handle the clicked city value
-                const key = "33180eb70b4778f17b8aaca9485bd8ad";
-                const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${clickedCity}&limit=5&appid=${key}`;
+    deleteButton.click(function () {
+        deleteCity(index);
+    });
 
-                fetch(queryURL1)
-                .then((response) => response.json())
-                .then((data) => {
-                const lat = data[0].lat;
-                const lon = data[0].lon;
-                const country = data[0].country;
-                console.log(data);    
+    cityButton.click(function () {
+        const clickedCity = $(this).text();
+        console.log("Clicked City:", clickedCity);
 
-                const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
-                // Fetch #2 to get the forecast
-                fetch(queryURL2)
-                .then((response) => response.json())
-                .then((data) => {
-                    
-                    function displayWeatherData() {
-                    const forecastContainer = $("#weatherForecast");
-    
-                      // Group data by day
-                    const groupedByDay = {};
-                    data.list.forEach((forecast) => {
-                        
-                        const dayOfWeek = dayjs(forecast.dt_txt).format("dddd DD");
-    
-                        
-                        if (dayjs(forecast.dt_txt).isAfter(dayjs(), "day")) {
-                        if (!groupedByDay[dayOfWeek]) {
-                            groupedByDay[dayOfWeek] = forecast;
-                        }
-                        }
-                    }); 
-                    
-                    for (const dayOfWeek in groupedByDay) {
-                        const dayContainer = $(
-                        `<div class="day-container"><h3>${dayOfWeek}</h3></div>`
-                        );
-                        const forecast = groupedByDay[dayOfWeek];
-    
-                        const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
-                        const tempKelvin = forecast.main.temp;
-                        const tempC = `${Math.round(tempKelvin - 273.15)}°C`;
-    
-                    
-                        dayContainer.append(
-                        `<div><img src="${imageUrl}" alt="Weather Icon"><br> ${forecast.weather[0].description} <br>Temp: ${tempC} |<br> Wind: ${forecast.wind.speed} m/ph | <br>Humidity: ${forecast.main.humidity}%</div> <hr>`
-                        );
-    
-                    
-                        forecastContainer.append(dayContainer);
+        const key = "33180eb70b4778f17b8aaca9485bd8ad";
+        const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${clickedCity}&limit=5&appid=${key}`;
+
+        fetch(queryURL1)
+        .then((response) => response.json())
+        .then((data) => {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            const country = data[0].country;
+            console.log(data);
+
+            const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+
+            fetch(queryURL2)
+            .then((response) => response.json())
+            .then((data) => {
+                function displayWeatherData() {
+                const forecastContainer = $("#weatherForecast");
+
+                const groupedByDay = {};
+                data.list.forEach((forecast) => {
+                    const dayOfWeek = dayjs(forecast.dt_txt).format("dddd DD");
+
+                    if (dayjs(forecast.dt_txt).isAfter(dayjs(), "day")) {
+                    if (!groupedByDay[dayOfWeek]) {
+                        groupedByDay[dayOfWeek] = forecast;
                     }
-                } 
-            
+                    }
+                });
+
+                for (const dayOfWeek in groupedByDay) {
+                    const dayContainer = $(
+                    `<div class="day-container"><h3>${dayOfWeek}</h3></div>`
+                    );
+                    const forecast = groupedByDay[dayOfWeek];
+
+                    const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+                    const tempKelvin = forecast.main.temp;
+                    const tempC = `${Math.round(tempKelvin - 273.15)}°C`;
+
+                    dayContainer.append(
+                    `<div><img src="${imageUrl}" alt="Weather Icon"><br> ${forecast.weather[0].description} <br>Temp: ${tempC} |<br> Wind: ${forecast.wind.speed} m/ph | <br>Humidity: ${forecast.main.humidity}%</div> <hr>`
+                    );
+
+                    forecastContainer.append(dayContainer);
+                }
+                }
+
                 const currentDate = dayjs().format("dddd DD MMM YYYY | HH:mm");
-    
+
                 const txt1 = $("<h2>").text(currentDate);
                 $("#today").append(txt1);
                 const location = `${clickedCity}, ${country}`;
@@ -209,23 +198,22 @@ $(document).ready(function () {
                 $("#today").append(tempC, wind, humidity);
 
                 displayWeatherData();
-                });
-            })
-            .catch((error)=> {
-                console.log(error);
             });
-
-            $("#today").empty();
-            $("#weatherForcast").empty();
+        })
+        .catch((error) => {
+            console.log(error);
         });
+
+        $("#today").empty();
+        $("#fiveDayForcast").empty();
     });
-            }
+    });
+}
 
-            function deleteCity(index) {
-                const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
-                savedCities.splice(index, 1);
-                localStorage.setItem("savedCities", JSON.stringify(savedCities));
-                displayCities(savedCities);
-            }
-
-        });
+function deleteCity(index) {
+    const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+    savedCities.splice(index, 1);
+    localStorage.setItem("savedCities", JSON.stringify(savedCities));
+    displayCities(savedCities);
+}
+});
